aa95130b4a5c1770f3f76a06a0360338c96e5fc2
湾仔 <Wendy@shiwanwandembp.home>
2024-05-08 22:47:54 +0100

    Post and delete comment

--- src/Components/SingleArticle.jsx
+++ src/Components/SingleArticle.jsx
@@ -1,14 +1,16 @@
-import { useParams } from "react-router-dom";
-import { getArticleById, getCommentByArticleId } from "./api";
-import { useState, useEffect } from "react";
+import { useNavigate, useParams } from "react-router-dom";
 import {
-	AiOutlineLike,
-	AiOutlineComment,
-	AiOutlineDislike,
-} from "react-icons/ai";
+	deleteCommentById,
+	getArticleById,
+	getCommentByArticleId,
+	postCommentByArticleId,
+} from "./api";
+import { useState, useEffect, useContext } from "react";
+import { AiOutlineComment } from "react-icons/ai";
 import CommentCard from "./CommentCard";
 import Badge from "@mui/material/Badge";
 import ArticleVotes from "./ArticleVotes";
+import { UserContext } from "../contexts/User";
 
 export default function SingleArticle() {
 	const { article_id } = useParams();
@@ -16,7 +18,52 @@ export default function SingleArticle() {
 	const [comments, setComments] = useState([]);
 	const [isArticleError, setIsArticleError] = useState(false);
 	const [isCommentError, setIsCommentError] = useState(false);
+	const [commentBody, setCommentBody] = useState("");
+	const [hideComment, setHideComment] = useState(true);
+	const [newComment, setNewComment] = useState({});
+	const [deletedCommentId, setDeletedCommentId] = useState("");
+	const [isDeleting, setIsDeleting] = useState(false);
+	const [isPosting, setIsPosting] = useState(false);
+	const { user } = useContext(UserContext);
+	const navigate = useNavigate();
 
+	const toggleComment = () => {
+		if (hideComment && !user.username) {
+			if (
+				confirm(
+					"You need to login to comment on this article, do you want to login now?"
+				)
+			) {
+				navigate("/news/users/login");
+			}
+		} else {
+			setHideComment(!hideComment);
+		}
+	};
+	const handleCommentSubmit = (e) => {
+		e.preventDefault();
+		if (commentBody.length <= 20) {
+			alert("Sorry, your comment need to be at least 20 characters");
+		} else {
+			if (confirm("Do you want to post this  comment?", commentBody)) {
+				setIsPosting(true);
+				const postObj = {};
+				postObj.username = user.username;
+				postObj.body = commentBody;
+				postCommentByArticleId(article_id, postObj)
+					.then(({ data }) => {
+						setIsPosting(false);
+						setNewComment(data.comment[0]);
+						alert("Your comment has been posted successfully");
+					})
+					.catch(() => {
+						alert(
+							"Something is wrong here and your comment post is not successful"
+						);
+					});
+			}
+		}
+	};
 	const {
 		title,
 		author,
@@ -27,6 +74,20 @@ export default function SingleArticle() {
 		body,
 	} = singleArticle;
 
+	const handleDelete = (comment_id) => {
+		if (confirm("Are you sure you want to delete this comment?")) {
+			setIsDeleting(true);
+			deleteCommentById(comment_id)
+				.then(() => {
+					setIsDeleting(false);
+					setDeletedCommentId(comment_id);
+					alert("Your comment has been deleted");
+				})
+				.catch(() => {
+					alert("Something is wrong and your delete was not successful");
+				});
+		}
+	};
 	useEffect(() => {
 		setIsArticleError(false);
 		getArticleById(article_id)
@@ -38,7 +99,7 @@ export default function SingleArticle() {
 			.catch(() => {
 				setIsArticleError(true);
 			});
-	}, [article_id, comment_count]);
+	}, [article_id, comment_count, commentBody]);
 
 	useEffect(() => {
 		setIsCommentError(false);
@@ -51,7 +112,8 @@ export default function SingleArticle() {
 			.catch(() => {
 				setIsCommentError(true);
 			});
-	}, [article_id, comment_count]);
+	}, [article_id, commentBody, newComment, deletedCommentId]);
+
 	if (isArticleError) {
 		return <h2>Something is wrong with this article....</h2>;
 	}
@@ -72,15 +134,43 @@ export default function SingleArticle() {
 				alt={`image of article {title}`}
 			/>
 			<p className="mx-5 my-5 text-lg">{body}</p>
+
 			<ArticleVotes votes={Number(votes)} article_id={article_id} />
-			<button className="my-3 btn-hl rounded-full block text-2xl p-2">
+
+			<button
+				className="my-3 btn-hl rounded-full block text-2xl p-2 "
+				onClick={toggleComment}
+			>
 				{" "}
 				+ Post new comment
 			</button>
+			<div
+				className={
+					hideComment
+						? "rounded-lg  w-full border-black border-1 border-solid m-3 hidden"
+						: "rounded-lg  w-full border-black border-1 border-solid m-3"
+				}
+			>
+				<form onSubmit={handleCommentSubmit}>
+					<input
+						type="text"
+						placeholder="Leave your comment here"
+						className="rounded-lg h-20 w-full border-black border-3 border-solid bg-orange-100 text-start"
+						value={commentBody}
+						onChange={(e) => {
+							setCommentBody(e.target.value);
+						}}
+					/>
+					<button className="my-5 rounded-lg text-2xl btn-hl p-2">
+						Submit
+					</button>
+				</form>
+			</div>
+			{isPosting && <span>Trying so hard to posting, be patient</span>}
 			<p className="inline-block">Comments</p>
 			<Badge
 				badgeContent={comment_count}
-				color="success"
+				color="info"
 				className="text-yellow-800 badge-color mx-3 font-bold"
 			>
 				<AiOutlineComment className="mx-2 size-8" />
@@ -91,7 +181,15 @@ export default function SingleArticle() {
 				<h2 className="text-2xl mb-20">There is no comment yet....</h2>
 			) : (
 				comments.map((comment) => {
-					return <CommentCard key={comment.comment_id} comment={comment} />;
+					return (
+						<CommentCard
+							key={comment.comment_id}
+							comment={comment}
+							handleDelete={handleDelete}
+							isDeleting={isDeleting}
+							deletedCommentId={deletedCommentId}
+						/>
+					);
 				})
 			)}
 		</div>
